0565d53ecb984315a769c9576a0099f414f7d99f
Maarja-Liisa Järve <mliisajarve@gmail.com>
2022-04-24 12:20:50 +0300

    added header and footer with routing

--- BakeryWebpage/src/app/app-routing.module.ts
+++ BakeryWebpage/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'home', component: MainPageComponent},
+  {path: 'contact', component: ContactPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BakeryWebpage/src/app/app.module.ts
+++ BakeryWebpage/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
