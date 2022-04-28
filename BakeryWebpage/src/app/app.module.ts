2298361676b17245cb50b1282f66fcb31284a5a3
Rauno <rraunoo@hmail.com>
2022-04-28 06:40:50 +0300

    made the form visible and changed app module

--- BakeryWebpage/src/app/app.module.ts
+++ BakeryWebpage/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +26,8 @@ import { SocialMediaComponent } from './social-media/social-media.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BakeryWebpage/src/app/contact-page/contact-page.component.ts
+++ BakeryWebpage/src/app/contact-page/contact-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
-  formOption: boolean = false
+  formOption: boolean = true
   constructor() { }
 
   ngOnInit(): void {
