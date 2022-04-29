acb5da7441b0f238ae3487b560187b131d9a97ab
Rauno <rraunoo@hmail.com>
2022-04-29 21:59:11 +0300

    worked on contact page functionality, footer fixed

--- BakeryWebpage/src/app/contact-form/contact-form.component.ts
+++ BakeryWebpage/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser'
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+  name: string = ''
+  email: string = ''
+  number: number | string = ''
+  info: string = ''
+  hideForm: string = ''
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  public onSubmit(e: Event) {
+    if (this.name && this.email && this.info !== '') {
+      e.preventDefault();
+      emailjs.sendForm('service_v9cqahs', 'template_75bkjcj', e.target as HTMLFormElement, 'QklSpd4CPoE0j1orE')
+
+        .then((result: EmailJSResponseStatus) => {
+          if (result.text == 'OK') {
+            this.name = ''
+            this.email = ''
+            this.number = ''
+            this.info = ''
+            this.hideForm = 'hidden'
+          }
+        })
+    } else {
+      alert('message not sent')
+    }
+  }
 }
