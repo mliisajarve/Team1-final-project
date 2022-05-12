ff3e3c9bbf6d18fe0b9d5ed8d43c09a9d059cb2f
Rauno <rraunoo@hmail.com>
2022-05-12 22:02:27 +0300

    navbar updated, mobile views

--- BakeryWebpage/src/app/header/header.component.ts
+++ BakeryWebpage/src/app/header/header.component.ts
@@ -10,29 +10,57 @@ export class HeaderComponent implements OnInit {
   dropdownStatus = 'dropdown-content';
   isShown: boolean = false;
   iconImg: string = 'fa fa-bars'
-
+  mobileToDesktopDropdown: string = ''
+  animation: string = ''
   constructor() {
    }
   
   ngOnInit(): void {
+    //default dropdown class value
+    this.mobileToDesktopDropdown = 'dropdown-hide'
   }
  
   showSearch() {
     this.isShown = ! this.isShown;
   }
+
+  // if hamburger is clicked / if navbar is opened and cross is clicked
   onHamburger(){
     if(window.screen.width< 525 && this.navbarMobile != 'navbarMobile'){
     this.navbarMobile = 'navbarMobile'
     this.iconImg = 'fa-solid fa-xmark'
+    this.animation=  'icon-animation'
+    setTimeout(()=>{
+      this.animation = ''
+    },100)
     } else {
       this.navbarMobile= ''
       this.iconImg = 'fa fa-bars'
+      this.animation = 'icon-animation'
+      setTimeout(()=>{
+        this.animation = ''
+      },100)
     }
   }
-  onAnyWhere(){
-    if(window.screen.width< 525)
+
+  // for mobile view resetting, if any link is clicked, then hiding mobile nav, and changing icon back to hamburger
+  onResetNavbar(){
+    if(window.screen.width< 525){
     this.navbarMobile= ''
     this.iconImg = 'fa fa-bars'
+    }
   }
-  
+  // if dropdown link is hovered or clicked in mobile view, changing between classes, by default is value dropdown-hide
+  onDropdown() {
+    if(window.screen.width< 525 && this.mobileToDesktopDropdown != 'mobileDropdown'){
+    this.mobileToDesktopDropdown = 'mobileDropdown'
+    
+    } else if(window.screen.width> 525){
+      this.mobileToDesktopDropdown = 'desktopDropdown'
+    }
+  }
+  // if mouse comes out of products list
+  leaveDropdown(){
+    this.mobileToDesktopDropdown = 'dropdown-hide'
+   }
 }
