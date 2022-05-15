6e2b14dd3289a5e31e61a9de16e7d51ed0833f3e
Maarja-Liisa Järve <mliisajarve@gmail.com>
2022-05-15 12:32:10 +0300

    Search engine update

--- BakeryWebpage/src/app/search-outcome-page/search-outcome-page.component.ts
+++ BakeryWebpage/src/app/search-outcome-page/search-outcome-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SearchInputComponent } from '../search-input/search-input.component';
 
 @Component({
   selector: 'app-search-outcome-page',
@@ -9,27 +7,12 @@ import { SearchInputComponent } from '../search-input/search-input.component';
 })
 export class SearchOutcomePageComponent implements OnInit {
   @Input() searchOutcome: string = "";
-  
-  inputValue2: string = "";
-  isShown: boolean = false;
 
-  constructor(private searchInput: SearchInputComponent, private router: Router) {
+  constructor() {
    }
 
 
   ngOnInit(): void {
-
-  }
-
-  showSearch() {
-    this.isShown = ! this.isShown;
-  }
-
-  onSearch(searchText:string):void {
-    this.router.navigate(['/result']);
-    this.isShown = false;
-    console.log(searchText)
-    this.inputValue2 = searchText;
   }
 
 }
